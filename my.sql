a82dbf4c96710264aca50c2344bc7d82c7e97078
Hemanth Kumar <digitalhemanth@gmail.com>
2021-11-28 23:47:09 +0530

    Update my.sql

--- my.sql
+++ my.sql
@@ -1,1 +1,14 @@
-select * grom here;
+
+
+create table pytest.emps(
+  Id serial primary key not Null,
+  Emp_Name varchar(250) not Null,
+  Tech int,
+  foreign key(Tech) references pytest.my_tech(tech_id)
+  on delete set Null
+);
+
+insert into pytest.emps(Emp_Name,Tech) values('Hemanth',2);
+
+
+select * from pytest.emps;
