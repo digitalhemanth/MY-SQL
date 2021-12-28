1f4b8f8e19f8a22ba570f90248c585a410d2bcd7
Hemanth Kumar <digitalhemanth@gmail.com>
2021-12-28 23:49:56 +0530

    Update SQL_Interview_questions.sql

--- SQL_Interview_questions.sql
+++ SQL_Interview_questions.sql
@@ -9,3 +9,26 @@ Ex.emp_name,
 Ex.salary from (select id , emp_name, salary,
 DENSE_RANK () OVER ( order by salary desc ) sal from pytest.emps ) Ex
 where Ex.sal = 2;
+
+
+--4. write the query to find the 2nd lowest salary in the employee table?
+
+select 
+Ex.id , 
+Ex.emp_name, 
+Ex.salary from (select id , emp_name, salary,
+DENSE_RANK () OVER ( order by salary ) sal from pytest.emps ) Ex
+where Ex.sal = 2;
+
+
+--write the query to find the distinct domain from the email column
+
+
+select substring(email from '@(.*)$') as domain, count(*)
+from  pytest.emps
+group by domain
+
+SELECT distinct(split_part (email,'@',2) ) as domain_name FROM pytest.emps 
+
+SELECT split_part (email,'@',2) as domain_name FROM pytest.emps group by domain_name
+
