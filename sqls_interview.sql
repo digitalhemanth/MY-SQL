579dd1860c5696bffc31c5046cb607b29138b9e1
Hemanth Kumar <digitalhemanth@gmail.com>
2022-02-11 23:58:15 +0530

    Update sqls_interview.sql

--- sqls_interview.sql
+++ sqls_interview.sql
@@ -11,3 +11,14 @@ ALTER TABLE EMPLOYEES DROP CONSTRAINT EMPLOYEES_PK;
 ALTER TABLE EMPLOYEES DROP PRIMARY KEY;
 
 
+CREATE TABLE contacts(
+   contact_id INT GENERATED ALWAYS AS IDENTITY,
+   customer_id INT,
+   contact_name VARCHAR(255) NOT NULL,
+   phone VARCHAR(15),
+   email VARCHAR(100),
+   PRIMARY KEY(contact_id),
+   CONSTRAINT fk_customer
+      FOREIGN KEY(customer_id) 
+	  REFERENCES customers(customer_id)
+)
