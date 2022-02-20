b70317388773b6e3b8acdbe3a83f0046518347c3
Hemanth Kumar <digitalhemanth@gmail.com>
2022-02-20 21:28:38 +0530

    Create sql_vs_nosql.sql

---
+++ sql_vs_nosql.sql
@@ -0,0 +1,11 @@
+Features of SQL
+Provide High-Performance Capabilities
+SQL is a powerful tool as it is highly compatible with all types of RDBMS like MySQL, SQL Server, Oracle Database, MS Access, etc. 
+Data Consistency: SQL adheres to ACID properties with a strict schema that ensures better data consistency. 
+Ensures Vertical Scalability
+Handle Large Transactions with efficiency
+Robust Security Measures like rigid schema, data consistency, data integrity, regular updates, etc. 
+Suitable for every type of organization – large or small. 
+SQL is easy to learn and manage
+Open Source Programming Language
+Supports Data Definition Language and Data Manipulation Language to query the databases
