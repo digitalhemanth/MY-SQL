686f019fd844cbabc4909e221a923eb5175a1bf1
Hemanth Kumar <digitalhemanth@gmail.com>
2022-05-18 00:34:42 +0530

    Update join_Script.sql

--- Employee_Database-SQL-Python/join_Script.sql
+++ Employee_Database-SQL-Python/join_Script.sql
@@ -77,9 +77,21 @@ inner join pytest.titles title
 on emp.emp_title_id = title.title_id
 inner join pytest.departments dept 
 on demp.dept_no = dept.dept_no
-group by dept.dept_name
-order by salary desc
 
+
+select 
+title.title as title ,
+dept.dept_name,  
+sum(sal.salary) over(partition by title order by title desc ) as sal
+from pytest.employees emp
+inner join pytest.salaries sal 
+on emp.emp_no = sal.emp_no
+inner join pytest.dept_emp demp 
+on emp.emp_no = demp.emp_no
+inner join pytest.titles title 
+on emp.emp_title_id = title.title_id
+inner join pytest.departments dept 
+on demp.dept_no = dept.dept_no
 
 
 
