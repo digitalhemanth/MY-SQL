7220e4b8a75f31b9dc1606f9e33f3747ff3201ca
Hemanth Kumar <digitalhemanth@gmail.com>
2022-05-26 00:07:14 +0530

    Update Joins_examples.sql

--- Joins_examples.sql
+++ Joins_examples.sql
@@ -42,3 +42,27 @@ create table pytest.Table_A
   LEFT JOIN PYTEST.TABLE_B B
   ON A.ID = B.ID
 
+--The RIGHT JOIN keyword returns all records from the right table (table2), and the matching records from the left table (table1). The result is 0 records from the left side, if there is no match.
+  SELECT 
+  A.ID,
+  A.NAMES,
+  B.ID,
+  B.CODE
+  FROM PYTEST.TABLE_A A
+  RIGHT JOIN PYTEST.TABLE_B B
+  ON A.ID = B.ID
+  
+  
+  
+  --
+Inner joins combine records from two tables whenever there are matching values in a field common to both tables.
+  SELECT 
+  A.ID,
+  A.NAMES,
+  B.ID,
+  B.CODE
+  FROM PYTEST.TABLE_A A
+  INNER JOIN PYTEST.TABLE_B B
+  ON A.ID = B.ID
+  
+  
