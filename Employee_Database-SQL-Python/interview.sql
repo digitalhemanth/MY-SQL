057987cbfd9844551980172e278dc0c7037b20a3
Hemanth Kumar <digitalhemanth@gmail.com>
2022-06-10 23:49:13 +0530

    Update interview.sql

--- Employee_Database-SQL-Python/interview.sql
+++ Employee_Database-SQL-Python/interview.sql
@@ -14,3 +14,34 @@ create table pytest.B(
 insert into pytest.B values(1,'1')
 
 
+select * from pytest.B;
+
+select 
+a.id,
+b.id
+from pytest.A a
+inner join pytest.B b on a.id = b.id
+
+
+select 
+a.id,
+b.id
+from pytest.A a
+left join pytest.B b on a.id = b.id
+
+
+select 
+a.id,
+b.id
+from pytest.A a
+right join pytest.B b on a.id = b.id
+
+
+select 
+a.id,
+b.id
+from pytest.A a
+cross join pytest.B b 
+
+
+
