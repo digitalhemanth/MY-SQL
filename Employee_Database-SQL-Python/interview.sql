2cb4a39f0115f5e951775e12b7c0e49558a59878
Hemanth Kumar <digitalhemanth@gmail.com>
2022-06-10 23:44:46 +0530

    Update interview.sql

--- Employee_Database-SQL-Python/interview.sql
+++ Employee_Database-SQL-Python/interview.sql
@@ -2,3 +2,15 @@ create table pytest.A(
   ID int,
   cols char(2)
 )
+
+insert into pytest.A values(1,'1')
+
+
+create table pytest.B(
+  ID int,
+  cols char(2)
+)
+
+insert into pytest.B values(1,'1')
+
+
