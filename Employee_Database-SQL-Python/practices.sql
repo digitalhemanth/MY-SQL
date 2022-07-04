42f1f874bcd18b12b455d56bba80694cd645ef28
Hemanth Kumar <digitalhemanth@gmail.com>
2022-07-04 23:37:49 +0530

    Update practices.sql

--- Employee_Database-SQL-Python/practices.sql
+++ Employee_Database-SQL-Python/practices.sql
@@ -5,3 +5,12 @@ create table pytest.Teams(
     away_team varchar(5),
     winners varchar(5)
 )
+
+insert into pytest.Teams values('BANG' , 'AND', 'AND');
+insert into pytest.Teams values('BANG' , 'KAR', 'AND');
+insert into pytest.Teams values('AND' , 'BANG', 'KAR');
+insert into pytest.Teams values('MAHA' , 'ASSA', 'BANG');
+insert into pytest.Teams values('CHA' , 'RAJA', 'JAI');
+insert into pytest.Teams values('PANJ' , 'KOAL', 'RAJ');
+insert into pytest.Teams values('ASSA' , 'JAI', 'BANG');
+insert into pytest.Teams values('BANG' , 'HIMA', 'AND');
